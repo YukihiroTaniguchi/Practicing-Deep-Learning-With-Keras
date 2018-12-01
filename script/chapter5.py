6b1bd0dbb9b40db9e317822b6822017db464b40e
谷口　幸宏 <y.softvalley@gmail.com>
2018-12-01 21:57:38 +0900

    commit_181201_pm_2

--- script/chapter5.py
+++ script/chapter5.py
@@ -35,3 +35,40 @@ model.compile(optimizer='rmsprop',
 model.fit(train_images, train_labels, epochs=5, batch_size=64)
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 test_acc
+
+model_no_max_pool = models.Sequential()
+model_no_max_pool.add(layers.Conv2D(32, (3, 3), activation='relu',
+                                    input_shape=(28, 28, 1)))
+model_no_max_pool.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model_no_max_pool.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model_no_max_pool.summary()
+
+import os, shutil
+pwd
+original_dataset_dir = '/Users/hoge/Practicing-Deep-Learning-With-Keras/data/kaggle_original_data'
+base_dir = '/Users/hoge/Practicing-Deep-Learning-With-Keras/data/cats_and_dogs_small'
+train_dir = os.path.join(base_dir, 'train')
+os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir, 'validation')
+os.mkdir(validation_dir)
+test_dir = os.path.join(base_dir, 'test')
+os.mkdir(test_dir)
+train_cats_dir = os.path.join(train_dir, 'cats')
+os.mkdir(train_cats_dir)
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+os.mkdir(train_dogs_dir)
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+os.mkdir(validation_cats_dir)
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+os.mkdir(validation_dogs_dir)
+test_cats_dir = os.path.join(test_dir, 'cats')
+os.mkdir(test_cats_dir)
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+os.mkdir(test_dogs_dir)
+fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+for fname in fnames:
+    src = os.path.join(original_dataset_dir, fname)
+    dst = os.path.join(train_cats_dir, fname)
+    shutil.copyfile(src, dst)
+    

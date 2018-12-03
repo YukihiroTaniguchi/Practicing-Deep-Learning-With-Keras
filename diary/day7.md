1f688c8256833e855d9dde12712fdb5aa0b3c859
谷口　幸宏 <y.softvalley@gmail.com>
2018-12-03 10:01:12 +0900

    commit_181203_am

--- diary/day7.md
+++ diary/day7.md
@@ -1,4 +1,59 @@
-### 181202(Sun)  
+### 181203(Mon)  
 p.135 - p.  
 chapter4.py : L75-L
 ##### *Remember me*  
+画像データの前処理ステップ
+1. 画像ファイルを読み込む
+2. JPEGファイルの内容をRGBのピクセルグリッドにデコードする。
+3. これらのピクセルを浮動小数点型のテンソルに変換する。
+4. ピクセル値(0~255)の尺度を[0, 1]の範囲の値にする。
+
+```python
+from keras.preprocessing.image import ImageDataGenerator
+#すべての画像を1/255でスケーリング
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,              #ターゲットディレクトリ
+    target_size=(150, 150), #すべての画像のサイズを 150 * 150 に変更
+    batch_size=20,          #バッチサイズ
+    class_mode='binary')    #binary_crossentropy を使用するため、二値のラベルが必要
+
+validation_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+#data batch shape: (20, 150, 150, 3)
+#labels batch shape: (20,)
+
+for data_batch, labels_batch in train_generator:
+    print('data batch shape:', data_batch.shape)
+    print('labels batch shape:', labels_batch.shape)
+    break
+
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=100, #2000サンプル / 1バッチ20
+                              epochs=300,
+                              validation_data=validation_generator,
+                              validation_steps=50) #1000サンプル / 1バッチ20
+
+```
+学習後、モデルを保存
+```python
+model.save('hoge_model.1')
+```
+
+サンプルが少ない場合、データ拡張を行い、サンプル数の水増しをする
+```python
+datagen = ImageDataGenerator(rotation_range=40, #画像をランダムに回転させる
+                             width_shift_range=0.2,
+                             height_shift_range=0.2,
+                             #画像を水平または垂直にランダムに並行移動させる範囲(幅全体または高さ全体の割合)
+                             shear_range=0.2, #等積変形をランダムに適用
+                             zoom_range=0.2, #図形の内側をランダムにズーム
+                             horizontal_flip=True, #画像の半分を水平方向にランダムに反転
+                             fill_mode='nearest') #新たに作成されたピクセルを埋めるための戦略
+
+```
--- script/chapter5.py
+++ script/chapter5.py
@@ -46,8 +46,8 @@ model_no_max_pool.summary()
 
 import os, shutil
 pwd
-original_dataset_dir = '/Users/hoge/Practicing-Deep-Learning-With-Keras/data/kaggle_original_data'
-base_dir = '/Users/hoge/Practicing-Deep-Learning-With-Keras/data/cats_and_dogs_small'
+original_dataset_dir = '/Users/hoge/Documents/Practicing-Deep-Learning-With-Keras'
+base_dir = '/Users/hoge/Documents/Practicing-Deep-Learning-With-Keras/data/cats_and_dogs_small'
 train_dir = os.path.join(base_dir, 'train')
 os.mkdir(train_dir)
 validation_dir = os.path.join(base_dir, 'validation')
@@ -107,3 +107,109 @@ print('total training dog images:', len(os.listdir(train_dogs_dir)))
 
 print('total validation cat images:', len(os.listdir(validation_cats_dir)))
 print('total validation dog images:', len(os.listdir(validation_dogs_dir)))
+
+print('total test cat images:', len(os.listdir(test_cats_dir)))
+print('total test dog images:', len(os.listdir(test_dogs_dir)))
+
+from keras import layers
+from keras import models
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(150, 150, 3)))
+
+model.add(layers.MaxPool2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPool2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPool2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPool2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.summary()
+
+from keras import optimizers
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+
+#5.2.4 データの前処理
+from keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,              #ターゲットディレクトリ
+    target_size=(150, 150), #すべての画像のサイズを 150 * 150 に変更
+    batch_size=20,          #バッチサイズ
+    class_mode='binary')    #binary_crossentropy を使用するため、二値のラベルが必要
+
+validation_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+for data_batch, labels_batch in train_generator:
+    print('data batch shape:', data_batch.shape)
+    print('labels batch shape:', labels_batch.shape)
+    break
+
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=100,
+                              epochs=30,
+                              validation_data=validation_generator,
+                              validation_steps=50)
+
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.legend()
+
+plt.show()
+
+datagen = ImageDataGenerator(rotation_range=40,
+                             width_shift_range=0.2,
+                             height_shift_range=0.2,
+                             shear_range=0.2,
+                             zoom_range=0.2,
+                             horizontal_flip=True,
+                             fill_mode='nearest')
+
+from keras.preprocessing import image
+
+fnames = [os.path.join(train_cats_dir, fname)
+          for fname in os.listdir(train_cats_dir)]
+
+img_path = fnames[3]
+img = image.load_img(img_path, target_size=(150, 150))
+x = image.img_to_array(img)
+
+x = x.reshape((1,) + x.shape)
+i = 0
+for batch in datagen.flow(x, batch_size=1):
+    plt.figure(i)
+    imgplot = plt.imshow(image.array_to_img(batch[0]))
+    i+= 1
+    if i % 4 == 0:
+        break

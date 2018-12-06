f059014ca7d928b5dcac01547387f1b838dd61e4
谷口　幸宏 <y.softvalley@gmail.com>
2018-12-07 08:10:20 +0900

    commit_181206_pm

--- script/chapter5.py
+++ script/chapter5.py
@@ -572,4 +572,16 @@ for layer_name, layer_activation in  zip(layer_names, activations):
     plt.title(layer_name)
     plt.grid(False)
     plt.imshow(display_grid, aspect='auto', cmap='viridis')
-    
+
+from keras.applications import VGG16
+from keras import backend as K
+
+model  = VGG16(weights='imagenet', include_top=False)
+
+layer_name = 'block3_conv1'
+filter_index = 0
+
+layer_output = model.get_layer(layer_name).output
+loss = K.mean(layer_output[:, :, :, filter_index])
+grads = K.gradients(loss, model.input)[0]
+grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)

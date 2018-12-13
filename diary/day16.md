5336d85a8c27908d2b26c47b964a388c74737842
y.taniguchi <y.softvalley@gmail.com>
2018-12-13 09:38:33 +0900

    commit_181213_am

---
+++ diary/day16.md
@@ -0,0 +1,109 @@
+### 181213(Thu)    
+p.217 - p.226
+chapter6.py : L514 - L667
+##### *Remember me*  
+時系列サンプルとそれらのターゲットを生成するデータジェネレーター  
+
+data : もとの浮動小数点数型のデータからなる配列  
+lookback : 入力データのtimestepsをいくつさかのぼるか  
+delay : Targetのtimestepsをいくつ進める化  
+min_index, max_index : 抽出するtimestepsの上限と下限を表すdata配列のインデックス。データの一部を検証とテストのためにとっておくのに役立つ  
+shuffle : サンプルをシャッフルするのか、それとも時間の順序で抽出するのか  
+batch_size : バッチひとつあたりのサンプル数  
+step : データをサンプリングする時の期間(単位はtimesteps)データポイントを1時間ごとに1つ抽出するために6に設定
+```python
+def generator(data, lookback, delay, min_index, max_index,
+              shuffle=False, batch_size=128, step=6):
+    if max_index is None:
+        max_index = len(data) - delay - 1
+    i = min_index + lookback
+    while 1:
+        if shuffle:
+            rows = np.random.randint(min_index + lookback, max_index,
+                                     size=batch_size)
+        else:
+            if i + batch_size >= max_index:
+                i = min_index + lookback
+            rows = np.arange(i, min(i + batch_size, max_index))
+            i += len(rows)
+
+        samples = np.zeros((len(rows),
+                            lookback // step,
+                            data.shape[-1]))
+        targets = np.zeros((len(rows),))
+        for j, row in enumerate(rows):
+            indices = range(rows[j] - lookback, rows[j], step)
+            samples[j] = data[indices]
+            targets[j] = data[rows[j] + delay][1]
+      yield samples, targets
+```
+訓練、検証、テストジェネレータの準備
+
+```python
+lookback = 1440
+step = 6
+delay = 144
+batch_size = 128
+
+#訓練ジェネレータ
+train_gen = generator(float_data,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=0,
+                      max_index=200000,
+                      shuffle=True,
+                      step=step,
+                      batch_size=batch_size)
+
+#検証ジェネレータ
+val_gen = generator(float_data,
+                    lookback=lookback,
+                    delay=delay,
+                    min_index=200001,
+                    max_index=300000,
+                    step=step,
+                    batch_size=batch_size)
+
+#テストジェネレータ
+test_gen = generator(float_data,
+                     lookback=lookback,
+                     delay=delay,
+                     min_index=300001,
+                     max_index=None,
+                     step=step,
+                     batch_size=batch_size)
+
+#検証データセット全体を調べるためにval_genから抽出するtimestepsの数
+val_steps = (300000 - 200001 - lookback) // batch_size
+
+#テストデータセット全体を調べるためにtest_genから抽出するtimestepsの数
+test_steps = (len(float_data) - 300001 - lookback) // batch_size
+```
+
+常識的なベースラインを設定する  
+Aクラスのインスタンス : 90%  
+Bクラスのインスタンス : 10%  
+-> 常識的なアプローチ 常にAクラスを予測する  
+-> 正解率90%
+これを超えるようにする
+
+RNNなどの複雑で計算負荷の高い機械学習モデルを調べる再には、単純で計算不可の低いモデルを試してみる
+-> 計算負荷の低い全結合モデル
+```python
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import RMSprop
+
+model = Sequential()
+model.add(layers.Flatten(input_shape=(lookback // step,
+                                      float_data.shape[-1])))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(1))
+
+model.compile(optimizer=RMSprop(), loss='mae')
+history = model.fit_generator(train_gen,
+                              steps_per_epoch=500,
+                              epochs=20,
+                              validation_data=val_gen,
+                              validation_steps=val_steps)
+```
--- script/chapter6.py
+++ script/chapter6.py
@@ -511,3 +511,157 @@ history = model.fit(input_train, y_train,
                     epochs=10,
                     batch_size=128,
                     validation_split=0.2)
+
+#6.3.1 気温予測問題
+#気象データセットのデータ調査
+import os
+
+#データセットが置かれているディレクトリ
+pwd
+data_dir = '/Users/yukihiro/Documents/practice/Practicing-Deep-Learning-With-Keras/data/jena_climate'
+fname = os.path.join(data_dir, 'jena_climate_2009_2016.csv')
+
+f = open(fname)
+data = f.read()
+f.close()
+lines = data.split('\n')
+header = lines[0].split(',')
+lines = lines[1:]
+
+print(header)
+print(len(lines))
+print(len(header))
+print(lines[0].split(','))
+
+#データの解析(420551行のデータをNumPy配列に変換)
+import numpy as np
+
+float_data = np.zeros((len(lines), len(header) -1))
+for i, line in enumerate(lines):
+    values = [float(x) for x in line.split(',')[1:]]
+    float_data[i, :] = values
+
+from matplotlib import pyplot as plt
+
+temp = float_data[:, 1] #気温(摂氏)
+plt.plot(range(len(temp)), temp)
+
+plt.plot(range(1440), temp[:1440])
+
+#6.3.2 データの準備
+mean = float_data[:200000].mean(axis=0)
+float_data -= mean
+std = float_data[:200000].std(axis=0)
+float_data /= std
+
+#6-33 時系列サンプルとそれらの目的値を生成するジェネレータ
+def generator(data, lookback, delay, min_index, max_index,
+              shuffle=False, batch_size=128, step=6):
+    if max_index is None:
+        max_index = len(data) - delay - 1
+    i = min_index + lookback
+    while 1:
+        if shuffle:
+            rows = np.random.randint(min_index + lookback, max_index,
+                                     size=batch_size)
+        else:
+            if i + batch_size >= max_index:
+                i = min_index + lookback
+            rows = np.arange(i, min(i + batch_size, max_index))
+            i += len(rows)
+
+        samples = np.zeros((len(rows),
+                            lookback // step,
+                            data.shape[-1]))
+        targets = np.zeros((len(rows),))
+        for j, row in enumerate(rows):
+            indices = range(rows[j] - lookback, rows[j], step)
+            samples[j] = data[indices]
+            targets[j] = data[rows[j] + delay][1]
+        yield samples, targets
+
+#訓練、検証、テストに使用するジェネレータの準備
+lookback = 1440
+step = 6
+delay = 144
+batch_size = 128
+
+#訓練ジェネレータ
+train_gen = generator(float_data,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=0,
+                      max_index=200000,
+                      shuffle=True,
+                      step=step,
+                      batch_size=batch_size)
+
+val_gen = generator(float_data,
+                    lookback=lookback,
+                    delay=delay,
+                    min_index=200001,
+                    max_index=300000,
+                    step=step,
+                    batch_size=batch_size)
+
+test_gen = generator(float_data,
+                     lookback=lookback,
+                     delay=delay,
+                     min_index=300001,
+                     max_index=None,
+                     step=step,
+                     batch_size=batch_size)
+
+#検証データセット全体を調べるためにval_genから抽出するtimestepsの数
+val_steps = (300000 - 200001 - lookback) // batch_size
+
+#テストデータセット全体を調べるためにtest_genから抽出するtimestepsの数
+test_steps = (len(float_data) - 300001 - lookback) // batch_size
+
+#6.3.3 機械学習とは別の、常識的なベースライン
+#常識的なベースラインのMAEを計算
+def evaluate_naive_method():
+    batch_maes = []
+    for step in range(val_steps):
+        samples, targets = next(val_gen)
+        preds = samples[:, -1, 1]
+        mae = np.mean(np.abs(preds - targets))
+        batch_maes.append(mae)
+    print(np.mean(batch_maes))
+
+evaluate_naive_method()
+celsius_mae = 0.29 * std[1]
+print(celsius_mae)
+
+#6.3.4 機械学習の基本的なアプローチ
+#全結合モデルの訓練と評価
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import RMSprop
+
+model = Sequential()
+model.add(layers.Flatten(input_shape=(lookback // step,
+                                      float_data.shape[-1])))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(1))
+
+model.compile(optimizer=RMSprop(), loss='mae')
+history = model.fit_generator(train_gen,
+                              steps_per_epoch=500,
+                              epochs=20,
+                              validation_data=val_gen,
+                              validation_steps=val_steps)
+
+#結果をプロット
+import matplotlib.pyplot as plt
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(loss))
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Traning loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training an validation loss')
+plot.legend()
